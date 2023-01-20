3ce3b959791f5b59b5f81cd5bee0ce8332c3dcfb
Walterhart <70658679+Walterhart@users.noreply.github.com>
2023-01-20 04:57:46 -0500

    Finish API functionality

--- controller/task.js
+++ controller/task.js
@@ -1,33 +1,90 @@
 
+const { find, findByIdAndUpdate } = require('../models/task')
 const Task = require('../models/task')
 
-const getAllTask = (req,res) =>{
-    res.send(" All Tasks")
+const getAllTask = async(req,res) =>{
+
+    try {
+        const tasks = await Task.find({})
+        res.status(200).send({tasks})
+        // if cant find item
+        if(!task){
+            return res.status(404). json({ms:`No task with id: ${taskID}`})
+        }
+        
+    } catch (error) {
+        res.status(500).json({msg:error})
+        
+    }
 }
 
-const getTaskById = (req,res) =>{
+const getTaskById = async(req,res) =>{
     // test  params
-    res.json({id:req.body.params.id})
+    try {
+        const {id:taskID} = req.params
+        const task = await Task.findOne({_id: taskID})
+        console.log(task)
+        if(!task){
+            return res.status(404).json({msg: `No task with id: ${taskID}`})
+        }
+        res.status(200).json({task})
+    } catch (error) {
+        res.status(500).json({msg:error})
+    }
 }
 
 
 const addTask = async (req,res) =>{
 
-    //test content added
-    const task = await Task.create(req.body)
+    
+    try {
+        // test content added
+        const task = await Task.create(req.body)
+        res.status(201).json({task})
+    } catch (error) {
+        // server error
+        res.status(500).json({msg:error})
+    }
    
-    res.status(201).json({task})
+   
     // test  params in postman
     // res.json({id:req.body.params.id})
 }
 
 
-const upadateTask = (req,res) =>{
-    res.send(" Task updated")
+const upadateTask = async(req,res) =>{
+
+   try {
+    const {id:taskID}= req.params
+    // new: new value, runValidators: run validators
+    const task = await Task.findOneAndUpdate({_id:taskID}, req.body, {new:true, runValidators:true,})
+    // test in postman find id and show bod
+    //res.status(200).json({id:taskId, data:req.body})
+    if(!task){
+        return res.status(404).json({msg: `No task with id: ${taskID}`})
+    }
+    res.status(200).json({task})
+   } catch (error) {
+        res.status(500).json({msg:error})
+   }
 }
 
-const deleteTask =(req,res) =>{
-    res.send("Task deleted")
+const deleteTask =async(req,res) =>{
+    try {
+        const{id:taskID} = req.params
+        const task = await Task.findOneAndDelete({_id:taskID})
+        if(!task){
+            return res.status(404).json({msg: `No task with id: ${taskID}`})
+        }
+        res.status(200).json({task})
+            
+            
+        
+    } catch (error) {
+        res.status(500).json({msg:error})
+        
+    }
+   
 }
 
 module.exports={
--- models/task.js
+++ models/task.js
@@ -2,11 +2,33 @@
 const mongoose = require('mongoose')
 
 // structure for documents
-// provie interface for api
+// provide interface for api
+// database collection format for insert document
+// only property in schema will be past to database!
+// any extra variable will be ignored
 const TaskSchema = new mongoose.Schema ({
-    name: String,
-    completed: Boolean,
+   
+    name: {
+
+        // validation
+        type: String,
+        // do not accept empty string and display custom message when empty
+        required: [true, 'Must provide a value'],
+
+        // trim extra white space
+        trim:true,
+
+        // max length
+        maxlength:[20, 'name can not be more than 20 characters'],
+    },
+    completed: {
+        type: Boolean,
+        // default value
+        default: false,
+    }
 })
+
+
 
 // need to export to controller
 module.exports = mongoose.model('Task', TaskSchema)
